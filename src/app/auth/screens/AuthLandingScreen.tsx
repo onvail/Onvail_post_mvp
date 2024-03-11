56e1de4d91fdde2b5f5822b830fbfc1b38053c1b
marvelous-ikechi <marvelike7@gmail.com>
2024-03-11 16:13:11 -0700

    updated authlanding page to use screen container

--- src/app/auth/screens/AuthLandingScreen.tsx
+++ src/app/auth/screens/AuthLandingScreen.tsx
@@ -6,6 +6,7 @@ import RoundedBtn from 'app/components/Buttons/RoundedBtn';
 import CustomText from 'app/components/Text/CustomText';
 import RowContainer from 'app/components/View/RowContainer';
 import {AuthStackParamList} from 'src/app/navigator/types/AuthStackParamList';
+import ScreenContainer from 'app/components/Screens/ScreenContainer';
 
 interface DividerProps {
   width: string;
@@ -20,32 +21,34 @@ type Props = NativeStackScreenProps<AuthStackParamList, 'AuthLandingScreen'>;
 
 const AuthLandingScreen: FunctionComponent<Props> = ({navigation}) => {
   return (
-    <View style={tw`bg-primary flex-1 justify-center px-4 items-center`}>
-      <CustomText style={tw`font-poppinsBold text-2xl w-1/2`}>
-        Blast into the music scenes
-      </CustomText>
-      <View style={tw`mx-6 mt-8`}>
-        <CustomText style={tw`text-center`}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin dui
-          enim, imperdiet non dictum vitae, aliquam quis dui. Morbi velit
-          turpis, dapibus mattis malesuada et, rhoncus vitae eros.
+    <ScreenContainer>
+      <View style={tw`flex-1 justify-center px-4 items-center`}>
+        <CustomText style={tw`font-poppinsBold text-2xl w-1/2`}>
+          Blast into the music scenes
         </CustomText>
+        <View style={tw`mx-6 mt-8`}>
+          <CustomText style={tw`text-center`}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin dui
+            enim, imperdiet non dictum vitae, aliquam quis dui. Morbi velit
+            turpis, dapibus mattis malesuada et, rhoncus vitae eros.
+          </CustomText>
+        </View>
+        <RowContainer style={tw`mt-6`}>
+          <DotDivider width="6" />
+          <DotDivider width="4" />
+          <DotDivider width="3" />
+          <DotDivider width="2" />
+        </RowContainer>
+        <RowContainer style={tw`mt-8 justify-between w-3/4`}>
+          <RoundedBtn title="Login" onPress={() => {}} borderColor="white" />
+          <RoundedBtn
+            title="Signup"
+            onPress={() => navigation.navigate('Signup')}
+            borderColor="purple"
+          />
+        </RowContainer>
       </View>
-      <RowContainer style={tw`mt-6`}>
-        <DotDivider width="6" />
-        <DotDivider width="4" />
-        <DotDivider width="3" />
-        <DotDivider width="2" />
-      </RowContainer>
-      <RowContainer style={tw`mt-8 justify-between w-3/4`}>
-        <RoundedBtn title="Login" onPress={() => {}} borderColor="white" />
-        <RoundedBtn
-          title="Signup"
-          onPress={() => navigation.navigate('Signup')}
-          borderColor="purple"
-        />
-      </RowContainer>
-    </View>
+    </ScreenContainer>
   );
 };
 
