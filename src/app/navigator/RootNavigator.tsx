bbfe65efb7f5ee8087cc8dad7eedc5c059d3d44a
marvelous-ikechi <marvelike7@gmail.com>
2024-03-11 15:52:06 -0700

    root navigator and type created

---
+++ src/app/navigator/RootNavigator.tsx
@@ -0,0 +1,17 @@
+import React, {FunctionComponent} from 'react';
+import {createStackNavigator} from '@react-navigation/stack';
+
+import {RootStackParamList} from './types/RootStackParamList';
+import AuthNavigator from './AuthNavigator';
+
+const RootStack = createStackNavigator<RootStackParamList>();
+
+const RootNavigator: FunctionComponent = () => {
+  return (
+    <RootStack.Navigator>
+      <RootStack.Screen name="AuthNavigator" component={AuthNavigator} />
+    </RootStack.Navigator>
+  );
+};
+
+export default RootNavigator;
