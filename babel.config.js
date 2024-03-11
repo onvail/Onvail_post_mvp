1ce2de8b6229506efaa031c04e2cef52c6b21ce7
marvelous-ikechi <marvelike7@gmail.com>
2024-03-11 12:02:11 -0700

    setup absolute import

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    ],
+  ],
 };
