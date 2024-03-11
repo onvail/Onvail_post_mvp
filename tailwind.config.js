7192c74a3dc153982c8e87109ee3d8c9fefd05be
marvelous-ikechi <marvelike7@gmail.com>
2024-03-11 12:22:45 -0700

    fonts setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,27 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: "#0E0E0E"
+        primary: "#0E0E0E",
+        white: "#FFFFFF",
       },
+      fontFamily: {
+        poppinsBlack: ['Poppins-Black'],
+        poppinsBlackItalic: ['Poppins-BlackItalic'],
+        poppinsBold: ['Poppins-Bold'],
+        poppinsBoldItalic: ['Poppins-BoldItalic'],
+        poppinsExtraBold: ['Poppins-ExtraBold'],
+        poppinsExtraBoldItalic: ['Poppins-ExtraBoldItalic'],
+        poppinsExtraLight: ['Poppins-ExtraLight'],
+        poppinsExtraLightItalic: ['Poppins-ExtraLightItalic'],
+        poppinsItalic: ['Poppins-Italic'],
+        poppinsLight: ['Poppins-Light'],
+        poppinsLightItalic: ['Poppins-LightItalic'],
+        poppinsMedium: ['Poppins-Medium'],
+        poppinsMediumItalic: ['Poppins-MediumItalic'],
+        poppinsRegular: ['Poppins-Regular'],
+        poppinsSemiBold: ['Poppins-SemiBold'],
+        poppinsSemiBoldItalic: ['Poppins-SemiBoldItalic'],
+      }
     },
   },
 };
