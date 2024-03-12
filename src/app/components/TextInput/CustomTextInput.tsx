ec883f13c059ff2f1cf15032a0a701cb96a14728
marvelous-ikechi <marvelike7@gmail.com>
2024-03-12 12:33:35 -0700

    added comments

--- src/app/components/TextInput/CustomTextInput.tsx
+++ src/app/components/TextInput/CustomTextInput.tsx
@@ -4,6 +4,12 @@ import tw from 'lib/tailwind';
 import {Colors} from 'app/styles/colors';
 import Icon from 'app/components/Icons/Icon';
 
+/**
+ * @Props extends TextInputProps ensuring components gets access to every TextInput prop.
+ * @inputType can either be undefined, Password or Text
+ * @passwordVisibility defines if password characters are scrambled (using ***) or not.
+ * @handlePasswordVisibility handles Icon press. In other words, toggles password visibility.
+ */
 interface Props extends TextInputProps {
   inputType?: 'Password' | 'Text';
   passwordVisibility?: boolean;
@@ -24,6 +30,8 @@ const CustomTextInput: FunctionComponent<Props> = ({
         style={tw` font-poppinsRegular text-sm w-10/11  text-primary`}
         {...props}
       />
+
+      {/* if @inputType is Password display eye icon */}
       {inputType === 'Password' && (
         <Icon
           name={passwordVisibility ? 'eye-off' : 'eye'}
