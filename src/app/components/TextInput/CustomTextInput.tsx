5611bf93d6be220912d9bb8a250ad15ee6b31c22
marvelous-ikechi <marvelike7@gmail.com>
2024-03-12 12:12:11 -0700

    added Icons and password state

--- src/app/components/TextInput/CustomTextInput.tsx
+++ src/app/components/TextInput/CustomTextInput.tsx
@@ -1,17 +1,37 @@
 import React, {FunctionComponent} from 'react';
-import {TextInput, TextInputProps} from 'react-native';
+import {TextInput, TextInputProps, View} from 'react-native';
 import tw from 'lib/tailwind';
 import {Colors} from 'app/styles/colors';
+import Icon from '../Icons/Icon';
 
-interface Props extends TextInputProps {}
+interface Props extends TextInputProps {
+  inputType?: 'Password' | 'Text';
+  passwordVisibility?: boolean;
+  handlePasswordVisibility?: () => void;
+}
 
-const CustomTextInput: FunctionComponent<Props> = ({...props}) => {
+const CustomTextInput: FunctionComponent<Props> = ({
+  inputType,
+  passwordVisibility,
+  handlePasswordVisibility,
+  ...props
+}) => {
   return (
-    <TextInput
-      placeholderTextColor={Colors.grey}
-      style={tw`bg-white rounded-md font-poppinsRegular h-12 text-sm px-3 mb-6 text-primary`}
-      {...props}
-    />
+    <View
+      style={tw`bg-white h-12 rounded-md mb-6 justify-between flex-row items-center px-3`}>
+      <TextInput
+        placeholderTextColor={Colors.grey}
+        style={tw` font-poppinsRegular text-sm w-10/11  text-primary`}
+        {...props}
+      />
+      {inputType === 'Password' && (
+        <Icon
+          name={passwordVisibility ? 'eye-off' : 'eye'}
+          color={Colors.purple}
+          onPress={handlePasswordVisibility}
+        />
+      )}
+    </View>
   );
 };
 
