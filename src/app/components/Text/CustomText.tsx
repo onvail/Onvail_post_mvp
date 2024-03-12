deb4e91a0f6694cf54d60f7a5ac6abe776b93998
marvelous-ikechi <marvelike7@gmail.com>
2024-03-11 17:24:42 -0700

    spread props across component

--- src/app/components/Text/CustomText.tsx
+++ src/app/components/Text/CustomText.tsx
@@ -1,15 +1,19 @@
 import React, {FunctionComponent} from 'react';
-import {TextStyle, Text} from 'react-native';
+import {TextStyle, Text, TextProps} from 'react-native';
 import tw from 'lib/tailwind';
 
-interface Props {
+interface Props extends TextProps {
   children: React.ReactNode;
   style?: TextStyle;
 }
 
-const CustomText: FunctionComponent<Props> = ({children, style}) => {
+const CustomText: FunctionComponent<Props> = ({children, style, ...props}) => {
   const customTextStyle = tw`font-poppinsRegular text-white`;
-  return <Text style={[customTextStyle, style]}>{children}</Text>;
+  return (
+    <Text style={[customTextStyle, style]} {...props}>
+      {children}
+    </Text>
+  );
 };
 
 export default CustomText;
