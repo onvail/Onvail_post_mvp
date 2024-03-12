7f85a4e277b1dd30ba58d36c98f94d2213a6ac05
marvelous-ikechi <marvelike7@gmail.com>
2024-03-11 17:59:49 -0700

    roles data created

---
+++ src/utils/roles.ts
@@ -0,0 +1,30 @@
+export const roles = [
+  {
+    key: 'Artiste',
+    value: 'artiste',
+  },
+  {
+    key: 'Producer',
+    value: 'producer',
+  },
+  {
+    key: 'A&R',
+    value: 'a&r',
+  },
+  {
+    key: 'Manager',
+    value: 'manager',
+  },
+  {
+    key: 'Label Representative',
+    value: 'label representative',
+  },
+  {
+    key: 'Music Content Creator',
+    value: 'cusic content creator',
+  },
+  {
+    key: 'Other',
+    value: 'other',
+  },
+];
