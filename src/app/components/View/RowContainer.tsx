eb457517b23eee454397ad3b4a09a681c79ec42c
marvelous-ikechi <marvelike7@gmail.com>
2024-03-13 15:36:35 -0700

    added styles

--- src/app/components/View/RowContainer.tsx
+++ src/app/components/View/RowContainer.tsx
@@ -8,7 +8,7 @@ interface Props {
 }
 
 const RowContainer: FunctionComponent<Props> = ({children, style}) => {
-  const rowContainerStyle = tw`flex-row`;
+  const rowContainerStyle = tw`flex-row items-center`;
   return <View style={[rowContainerStyle, style]}>{children}</View>;
 };
 
