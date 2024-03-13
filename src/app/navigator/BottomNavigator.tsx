9518bd7b1e339b128213af010ca10805f710a5cc
marvelous-ikechi <marvelike7@gmail.com>
2024-03-13 14:17:59 -0700

    added profile to bottom navigator

--- src/app/navigator/BottomNavigator.tsx
+++ src/app/navigator/BottomNavigator.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {BottomTabParamList} from './types/BottomTabParamList';
 import Home from 'app/home/screens/Home';
+import TabBarIcon from 'components/Icons/TabBarIcon';
+import {RouteProp} from '@react-navigation/native';
+import Profile from 'app/profile/screen/Profile';
+import {Colors} from '../styles/colors';
 
 const Tab = createBottomTabNavigator<BottomTabParamList>();
 
@@ -15,11 +19,32 @@ const tabs: ScreenDef[] = [
     name: 'Home',
     component: Home,
   },
+  {
+    name: 'Profile',
+    component: Profile,
+  },
 ];
+
+type TabBarProps = {
+  focused: boolean;
+  color: string;
+};
+
+const renderTabBarIcon =
+  (route: RouteProp<BottomTabParamList>) => (props: TabBarProps) =>
+    <TabBarIcon {...props} route={route} />;
 
 const BottomTabNavigator = () => {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({route}) => ({
+        tabBarIcon: renderTabBarIcon(route),
+        tabBarLabel: () => null,
+        headerShown: false,
+        tabBarStyle: {
+          backgroundColor: Colors.primary,
+        },
+      })}>
       {tabs.map(({name, component}) => (
         <Tab.Screen key={name} name={name} component={component} />
       ))}
