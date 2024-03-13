e60e9af545620f3c4eadfc27328abfc9e77e8000
marvelous-ikechi <marvelike7@gmail.com>
2024-03-13 15:35:34 -0700

    new icons

--- src/app/components/Icons/generalIcons.ts
+++ src/app/components/Icons/generalIcons.ts
@@ -1,10 +1,14 @@
 import {SvgProps} from 'react-native-svg';
+import LogoSvg from 'src/assets/svg/logo.svg';
 import HomeSvg from 'src/assets/svg/home.svg';
 import ProfileSvg from 'src/assets/svg/profile.svg';
+import NotificationBell from 'src/assets/svg/bell.svg';
 
-type GeneralType = 'Home' | 'Profile';
+type GeneralType = 'Home' | 'Profile' | 'Logo' | 'NotificationBell';
 
 export const generalIcon: Record<GeneralType, React.FC<SvgProps>> = {
   ['Home']: HomeSvg,
   ['Profile']: ProfileSvg,
+  ['Logo']: LogoSvg,
+  ['NotificationBell']: NotificationBell,
 };
