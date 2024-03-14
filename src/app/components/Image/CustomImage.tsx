3a5ef2c938a0e0618d43d000ac98ae8ecafdb276
marvelous-ikechi <marvelike7@gmail.com>
2024-03-14 11:54:48 -0700

    removed unnecessary components

--- src/app/components/Image/CustomImage.tsx
+++ src/app/components/Image/CustomImage.tsx
@@ -3,19 +3,11 @@ import FastImage, {FastImageProps} from 'react-native-fast-image';
 
 interface Props extends FastImageProps {
   uri: string;
-  height: number;
-  width: number;
 }
 
-const CustomImage: FunctionComponent<Props> = ({
-  uri,
-  height,
-  width,
-  ...props
-}) => {
+const CustomImage: FunctionComponent<Props> = ({uri, ...props}) => {
   return (
     <FastImage
-      style={{width: width, height: height}}
       source={{
         uri: uri,
         headers: {Authorization: 'someAuthToken'},
