573b9ace378db15f88cc9bec605b3a2b1a770b5f
marvelous-ikechi <marvelike7@gmail.com>
2024-03-14 13:15:14 -0700

    new svgs added

--- src/app/components/Icons/generalIcons.ts
+++ src/app/components/Icons/generalIcons.ts
@@ -3,12 +3,22 @@ import LogoSvg from 'src/assets/svg/logo.svg';
 import HomeSvg from 'src/assets/svg/home.svg';
 import ProfileSvg from 'src/assets/svg/profile.svg';
 import NotificationBell from 'src/assets/svg/bell.svg';
+import Heart from 'src/assets/svg/heart.svg';
+import Comment from 'src/assets/svg/comment.svg';
 
-type GeneralType = 'Home' | 'Profile' | 'Logo' | 'NotificationBell';
+type GeneralType =
+  | 'Home'
+  | 'Profile'
+  | 'Logo'
+  | 'NotificationBell'
+  | 'Heart'
+  | 'Comment';
 
 export const generalIcon: Record<GeneralType, React.FC<SvgProps>> = {
   ['Home']: HomeSvg,
   ['Profile']: ProfileSvg,
   ['Logo']: LogoSvg,
   ['NotificationBell']: NotificationBell,
+  ['Heart']: Heart,
+  ['Comment']: Comment,
 };
