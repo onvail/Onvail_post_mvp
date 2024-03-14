4b0c4330956d9d7bea6123a00f8c4be442a6c9d1
marvelous-ikechi <marvelike7@gmail.com>
2024-03-14 13:32:53 -0700

    UserHeader completed

---
+++ src/app/components/Posts/UserHeader.tsx
@@ -0,0 +1,38 @@
+import React, {FunctionComponent} from 'react';
+import {TouchableOpacity} from 'react-native';
+import CustomImage from 'components/Image/CustomImage';
+import tw from 'src/lib/tailwind';
+import CustomText from 'components/Text/CustomText';
+import RowContainer from '../View/RowContainer';
+
+interface User {
+  name: string;
+  uri: string;
+  handleFollowBtnPress: () => void;
+}
+
+const UserHeader: FunctionComponent<User> = ({
+  name,
+  uri,
+  handleFollowBtnPress,
+}) => {
+  return (
+    <RowContainer style={tw`px-4 justify-between`}>
+      <RowContainer>
+        <CustomImage
+          uri={uri}
+          style={tw`h-9 w-9 rounded-full`}
+          resizeMode="cover"
+        />
+        <CustomText style={tw`ml-3`}>{name}</CustomText>
+      </RowContainer>
+      <TouchableOpacity
+        onPress={() => handleFollowBtnPress()}
+        style={tw`border rounded-full border-white px-5 py-1`}>
+        <CustomText>Follow</CustomText>
+      </TouchableOpacity>
+    </RowContainer>
+  );
+};
+
+export default UserHeader;
