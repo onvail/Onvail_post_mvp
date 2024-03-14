131ad0bf6379c50d2563611c4c9b43807f66b2d8
marvelous-ikechi <marvelike7@gmail.com>
2024-03-14 13:33:19 -0700

    sample posts created

---
+++ src/utils/posts.ts
@@ -0,0 +1,42 @@
+export interface Post {
+  key: string;
+  profileImage: string;
+  userName: string;
+  postImage: string;
+  postText: string;
+  commentCount?: number;
+  likeCount?: number;
+}
+
+export const posts: Post[] = [
+  {
+    key: '1',
+    profileImage:
+      'https://www.cityheadshots.com/uploads/5/1/2/1/5121840/editor/girlfriend.jpg?1643119015',
+    userName: 'Sabrina Carpenter',
+    postText:
+      'Sleeping with the fishes made the meats run dry in her episconic sagacious vibe. Listen and be blessed',
+    postImage:
+      'https://indiater.com/wp-content/uploads/2021/06/Free-Music-Album-Cover-Art-Banner-Photoshop-Template-990x990.jpg',
+  },
+  {
+    key: '2',
+    profileImage:
+      'https://www.cityheadshots.com/uploads/5/1/2/1/5121840/editor/girlfriend.jpg?1643119015',
+    userName: 'Sabrina Carpenter',
+    postText:
+      'Sleeping with the fishes made the meats run dry in her episconic sagacious vibe. Listen and be blessed',
+    postImage:
+      'https://miro.medium.com/v2/resize:fit:720/format:webp/1*EBOL4lka5QjcYoxj6AHp-g.png',
+  },
+  {
+    key: '3',
+    profileImage:
+      'https://www.cityheadshots.com/uploads/5/1/2/1/5121840/editor/girlfriend.jpg?1643119015',
+    userName: 'Sabrina Carpenter',
+    postText:
+      'Sleeping with the fishes made the meats run dry in her episconic sagacious vibe. Listen and be blessed',
+    postImage:
+      'https://img.buzzfeed.com/buzzfeed-static/complex/images/bebllwzjpsujz9ffwp6s/tyler-the-creator-scum-fuck-flower-boy-cover.png?downsize=920:*&output-format=auto&output-quality=auto',
+  },
+];
