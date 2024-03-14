9ac45da507d54d507da94a2914f791317edc367e
marvelous-ikechi <marvelike7@gmail.com>
2024-03-14 13:33:10 -0700

    updated styles and added PostCard

--- src/app/home/screens/Home.tsx
+++ src/app/home/screens/Home.tsx
@@ -1,6 +1,7 @@
 import React, {FunctionComponent} from 'react';
 import {View} from 'react-native';
 import {generalIcon} from 'src/app/components/Icons/generalIcons';
+import PostCard from 'src/app/components/Posts/PostCard';
 import ScreenContainer from 'src/app/components/Screens/ScreenContainer';
 import Status from 'src/app/components/Status/Status';
 import RowContainer from 'src/app/components/View/RowContainer';
@@ -11,13 +12,18 @@ const Home: FunctionComponent = () => {
   const NotificationBellSvg = generalIcon.NotificationBell;
   return (
     <ScreenContainer>
-      <View style={tw`mx-3 flex-1`}>
-        <RowContainer style={tw`justify-between`}>
-          <LogoSvg />
-          <NotificationBellSvg style={tw`mr-5`} />
-        </RowContainer>
-        <View style={tw`mt-4`}>
-          <Status />
+      <View style={tw`flex-1`}>
+        <View style={tw`mx-3`}>
+          <RowContainer style={tw`justify-between`}>
+            <LogoSvg />
+            <NotificationBellSvg style={tw`mr-5`} />
+          </RowContainer>
+          <View style={tw`mt-4`}>
+            <Status />
+          </View>
+        </View>
+        <View style={tw`mt-8 flex-1 `}>
+          <PostCard />
         </View>
       </View>
     </ScreenContainer>
