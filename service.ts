43ac8ac20c9c0496c606adce60438752285354ef
marvelous-ikechi <marvelike7@gmail.com>
2024-03-14 15:57:26 -0700

    added event listeners

--- service.ts
+++ service.ts
@@ -1,5 +1,24 @@
-import TrackPlayer from 'react-native-track-player';
+import TrackPlayer, {Event} from 'react-native-track-player';
 
 module.exports = async () => {
   await TrackPlayer.setupPlayer();
+  try {
+    TrackPlayer.addEventListener(Event.RemotePlay, () => {
+      TrackPlayer.play();
+    });
+
+    TrackPlayer.addEventListener(Event.RemotePause, () => {
+      TrackPlayer.pause();
+    });
+
+    TrackPlayer.addEventListener(Event.RemoteNext, () => {
+      TrackPlayer.skipToNext();
+    });
+
+    TrackPlayer.addEventListener(Event.RemotePrevious, () => {
+      TrackPlayer.skipToPrevious();
+    });
+
+    TrackPlayer.addEventListener(Event.RemoteStop, () => {});
+  } catch (error) {}
 };
