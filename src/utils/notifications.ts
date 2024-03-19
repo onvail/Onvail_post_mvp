20824a44e08918bdf56ee6c1247e161e117dd34b
marvelous-ikechi <marvelike7@gmail.com>
2024-03-19 13:25:29 -0700

    sample notification data created

---
+++ src/utils/notifications.ts
@@ -0,0 +1,11 @@
+import {NotificationProps} from 'src/app/notifications/components/NotificationCard';
+
+export const notification: NotificationProps[] = [
+  {
+    subject: 'Congrats! Awesome Hc',
+    message: 'ust joined your community.',
+    timeStamp: '24s',
+    imageUri:
+      'https://resizing.flixster.com/xkP-QzPdNnU1Q8KQuBB6Q0YCeTU=/218x280/v2/https://resizing.flixster.com/-XZAfHZM39UwaGJIFWKAE8fS0ak=/v3/t/assets/371287_v9_bc.jpg',
+  },
+];
