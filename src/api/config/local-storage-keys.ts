7e57de4ac160b43bc4166037b7cc832bd69b4a76
marvelous-ikechi <marvelike7@gmail.com>
2024-04-12 20:15:08 -0700

    updated local storage keys

--- src/api/config/local-storage-keys.ts
+++ src/api/config/local-storage-keys.ts
@@ -2,7 +2,6 @@ export default {
   accessToken: 'access-token',
   firstLaunch: 'first-launch',
   refreshToken: 'refresh-token',
-  tokenType: 'token-type',
   appLaunched: 'app-launched',
   countryCode: 'country-code',
   lastLogin: 'last-login',
@@ -10,9 +9,6 @@ export default {
   useBiometrics: 'use-biometrics',
   pushNotificationEnabled: 'push-notification-enabled',
   pushNotificationLastPrompt: 'push-notification-last-prompt',
-  loginDetails: 'login-details',
   deviceName: 'device-name',
   profileImage: 'profile-image',
-  removeReferalCard: 'remove-refferal-card',
-  selectedAccountNumber: 'selected-account-number',
 };
