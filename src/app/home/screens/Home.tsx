4b185519f4ebbc0ad8fbe98de87cbe1b79ebdb80
marvelous-ikechi <marvelike7@gmail.com>
2024-04-15 10:40:06 -0700

    added Parties and Feeds toggle

--- src/app/home/screens/Home.tsx
+++ src/app/home/screens/Home.tsx
@@ -1,5 +1,5 @@
-import React, {FunctionComponent} from 'react';
-import {View} from 'react-native';
+import React, {FunctionComponent, useState} from 'react';
+import {TouchableOpacity, View} from 'react-native';
 import {generalIcon} from 'src/app/components/Icons/generalIcons';
 import PostCard from 'src/app/components/Posts/PostCard';
 import ScreenContainer from 'src/app/components/Screens/ScreenContainer';
@@ -8,12 +8,16 @@ import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import RowContainer from 'src/app/components/View/RowContainer';
 import {BottomTabParamList} from 'src/app/navigator/types/BottomTabParamList';
 import tw from 'src/lib/tailwind';
+import CustomText from 'src/app/components/Text/CustomText';
 
 type Props = NativeStackScreenProps<BottomTabParamList, 'Home'>;
 
 const Home: FunctionComponent<Props> = ({navigation}) => {
   const LogoSvg = generalIcon.Logo;
   const NotificationBellSvg = generalIcon.NotificationBell;
+  type Tabs = 'Parties' | 'Feeds';
+  const [selectedTab, setSelectedTab] = useState<Tabs>('Parties');
+  const tab: Tabs[] = ['Parties', 'Feeds'];
   return (
     <ScreenContainer>
       <View style={tw`flex-1`}>
@@ -21,13 +25,6 @@ const Home: FunctionComponent<Props> = ({navigation}) => {
           <RowContainer style={tw`justify-between`}>
             <LogoSvg />
             <RowContainer>
-              {/* <AddSqurareSvg
-                onPress={() =>
-                  navigation.navigate('MainAppNavigator', {
-                    screen: 'CreateNewPost',
-                  })
-                }
-              /> */}
               <NotificationBellSvg
                 onPress={() =>
                   navigation.navigate('MainAppNavigator', {
@@ -38,11 +35,29 @@ const Home: FunctionComponent<Props> = ({navigation}) => {
               />
             </RowContainer>
           </RowContainer>
-          <View style={tw`mt-4`}>
-            <Status />
-          </View>
+          <Status />
         </View>
-        <View style={tw`mt-8 flex-1 `}>
+        <RowContainer
+          style={tw`items-center w-2/3 p-2 self-center h-14 rounded-full justify-center my-6 bg-grey6`}>
+          {tab.map((item, index) => {
+            return (
+              <TouchableOpacity
+                onPress={() => setSelectedTab(item)}
+                style={tw`bg-${
+                  item === selectedTab ? 'white' : 'transparent'
+                } h-11 w-1/2 rounded-full justify-center items-center`}
+                key={index}>
+                <CustomText
+                  style={tw`text-${
+                    item === selectedTab ? 'primary' : 'white'
+                  } text-lg font-medium`}>
+                  {item}
+                </CustomText>
+              </TouchableOpacity>
+            );
+          })}
+        </RowContainer>
+        <View style={tw`flex-1 `}>
           <PostCard
             handleJoinPartyBtnPress={() =>
               navigation.navigate('MainAppNavigator', {
