3a6446abfbfd2ddf91fbcc1bce0453b2e0f1f4b5
marvelous-ikechi <marvelike7@gmail.com>
2024-04-16 16:13:32 -0700

    added GestureHandler to root to handle draggable flatlist

--- App.tsx
+++ App.tsx
@@ -2,12 +2,15 @@ import React, {FunctionComponent} from 'react';
 import {View} from 'react-native';
 import tw from 'lib/tailwind';
 import Main from 'app/Main';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
 
 const App: FunctionComponent = () => {
   return (
-    <View style={tw`flex-1`}>
-      <Main />
-    </View>
+    <GestureHandlerRootView style={tw`flex-1`}>
+      <View style={tw`flex-1`}>
+        <Main />
+      </View>
+    </GestureHandlerRootView>
   );
 };
 
