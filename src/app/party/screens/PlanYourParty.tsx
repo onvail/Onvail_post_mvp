2f2e4639467df40c5467e1ea50e51468315800cb
marvelous-ikechi <marvelike7@gmail.com>
2024-04-17 10:47:19 -0700

    added KeyboardAwareScrollView

--- src/app/party/screens/PlanYourParty.tsx
+++ src/app/party/screens/PlanYourParty.tsx
@@ -1,5 +1,12 @@
 import React, {FunctionComponent, useState} from 'react';
-import {Pressable, ScrollView, View, TouchableOpacity} from 'react-native';
+import {
+  Pressable,
+  View,
+  TouchableOpacity,
+  KeyboardAvoidingView as RNKeyboardAvoidingView,
+  KeyboardAvoidingViewProps as RNKeyboardAvoidingViewProps,
+  Platform,
+} from 'react-native';
 import {generalIcon} from 'src/app/components/Icons/generalIcons';
 import ScreenContainer from 'src/app/components/Screens/ScreenContainer';
 import CustomText from 'src/app/components/Text/CustomText';
@@ -18,6 +25,20 @@ import {DocumentPickerResponse} from 'react-native-document-picker';
 import {truncateText} from 'src/utils/utilities';
 import ErrorText from 'src/app/components/Text/ErrorText';
 import VotingPoll from '../components/VotingPoll';
+import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
+
+const BEHAVIOR = Platform.OS === 'ios' ? 'padding' : undefined;
+
+const KeyboardAvoidingView = ({
+  style,
+  ...props
+}: RNKeyboardAvoidingViewProps) => (
+  <RNKeyboardAvoidingView
+    style={[tw`flex-1`, style]}
+    behavior={BEHAVIOR}
+    {...props}
+  />
+);
 
 const PlanYourParty: FunctionComponent = () => {
   const [isCalendarVisible, setIsCalendarVisible] = useState<boolean>(false);
@@ -53,8 +74,6 @@ const PlanYourParty: FunctionComponent = () => {
     console.log('data', data);
   };
 
-  console.log(errors);
-
   const {selectDocument} = useDocumentPicker();
 
   const handleSelectPhoto = async (action: 'openCamera' | 'openPicker') => {
@@ -75,193 +94,195 @@ const PlanYourParty: FunctionComponent = () => {
 
   return (
     <ScreenContainer screenHeader="Plan your party" goBack>
-      <ScrollView
-        showsVerticalScrollIndicator={false}
-        style={tw`mx-4 flex-1 mt-6 mb-20`}>
-        <View style={tw`  `}>
-          <RowContainer style={tw`mb-2 justify-between`}>
-            <CustomText>Title</CustomText>
-            <ErrorText>{errors?.partyName?.message}</ErrorText>
-          </RowContainer>
-          <Controller
-            control={control}
-            rules={{
-              required: 'Title is required',
-            }}
-            render={({field: {onChange, onBlur, value}}) => (
-              <CustomTextInput
-                placeholder="I am on Onvail"
-                backgroundColor="transparent"
-                borderColor={'#717171'}
-                borderWidth="1"
-                style={tw`text-white w-full`}
-                onChangeText={onChange}
-                onBlur={onBlur}
-                value={value}
-              />
-            )}
-            name="partyName"
-          />
-        </View>
-        <View style={tw``}>
-          <RowContainer style={tw`mb-2 justify-between`}>
-            <CustomText>About your party</CustomText>
-            <ErrorText>{errors?.partyDesc?.message}</ErrorText>
-          </RowContainer>
-          <Controller
-            control={control}
-            rules={{
-              required: 'Description is required',
-            }}
-            render={({field: {onChange, onBlur, value}}) => (
-              <CustomTextInput
-                placeholder="I am on Onvail"
-                backgroundColor="transparent"
-                borderColor={'#717171'}
-                borderWidth="1"
-                height={120}
-                onChangeText={onChange}
-                onBlur={onBlur}
-                value={value}
-                multiline={true}
-                textAlignVertical="top"
-                style={tw`h-11/12 w-full text-white`}
-              />
-            )}
-            name="partyDesc"
-          />
-        </View>
-        <View style={tw``}>
-          <ErrorText>{errors?.albumPicture?.message}</ErrorText>
-          <Controller
-            control={control}
-            rules={{
-              required: 'Album image is required',
-            }}
-            render={({field: {onChange}}) => (
-              <Pressable
-                onPress={async () => {
-                  const image = await handleSelectPhoto('openPicker');
-                  console.log(image);
-                  onChange(image);
-                }}
-                style={tw`border border-grey2 h-50  items-center justify-center rounded-md`}>
-                {selectedImage ? (
-                  <CustomImage
-                    resizeMode="contain"
-                    uri={selectedImage}
-                    style={tw`h-50 w-90 rounded-md`}
-                  />
-                ) : (
-                  <RowContainer>
-                    <GalleryThumbnailSvg />
-                    <CustomText style={tw`ml-3`}>Add an image</CustomText>
-                  </RowContainer>
-                )}
-              </Pressable>
-            )}
-            name="albumPicture"
-          />
-        </View>
-        <View style={tw`mt-3`}>
-          <ErrorText>{errors?.songs?.message}</ErrorText>
-          <Controller
-            control={control}
-            rules={{
-              required: 'Select a song',
-            }}
-            render={({field: {onChange}}) => (
-              <>
-                <FormSelector
-                  description="Add music file"
-                  instruction="(max 500mb)"
-                  icon="library-music"
-                  onPress={() => {
-                    const data = handleSelectMusicFile();
-                    const items = data.then(res =>
-                      res?.map(item => ({
-                        name: item.name,
-                        file_url: item.uri,
-                      })),
-                    );
-                    onChange(items);
-                  }}
+      <KeyboardAwareScrollView style={tw`flex-1`}>
+        <View style={tw`mx-4 flex-1 mt-12 mb-10`}>
+          <View style={tw`  `}>
+            <RowContainer style={tw`mb-2 justify-between`}>
+              <CustomText>Title</CustomText>
+              <ErrorText>{errors?.partyName?.message}</ErrorText>
+            </RowContainer>
+            <Controller
+              control={control}
+              rules={{
+                required: 'Title is required',
+              }}
+              render={({field: {onChange, onBlur, value}}) => (
+                <CustomTextInput
+                  placeholder="I am on Onvail"
+                  backgroundColor="transparent"
+                  borderColor={'#717171'}
+                  borderWidth="1"
+                  style={tw`text-white w-full`}
+                  onChangeText={onChange}
+                  onBlur={onBlur}
+                  value={value}
                 />
-                {selectedSongs?.map((item, _) => {
-                  return (
-                    <CustomText
-                      style={tw`text-2xs mt-1 text-purple`}
-                      key={item.name + item?.uri}>
-                      {truncateText(item.name!)}
-                    </CustomText>
-                  );
-                })}
-              </>
-            )}
-            name="songs"
-          />
-        </View>
-        <View style={tw``}>
-          <ErrorText>{errors?.date?.message}</ErrorText>
+              )}
+              name="partyName"
+            />
+          </View>
+          <View style={tw``}>
+            <RowContainer style={tw`mb-2 justify-between`}>
+              <CustomText>About your party</CustomText>
+              <ErrorText>{errors?.partyDesc?.message}</ErrorText>
+            </RowContainer>
+            <Controller
+              control={control}
+              rules={{
+                required: 'Description is required',
+              }}
+              render={({field: {onChange, onBlur, value}}) => (
+                <CustomTextInput
+                  placeholder="I am on Onvail"
+                  backgroundColor="transparent"
+                  borderColor={'#717171'}
+                  borderWidth="1"
+                  height={120}
+                  onChangeText={onChange}
+                  onBlur={onBlur}
+                  value={value}
+                  multiline={true}
+                  textAlignVertical="top"
+                  style={tw`h-11/12 w-full text-white`}
+                />
+              )}
+              name="partyDesc"
+            />
+          </View>
+          <View style={tw``}>
+            <ErrorText>{errors?.albumPicture?.message}</ErrorText>
+            <Controller
+              control={control}
+              rules={{
+                required: 'Album image is required',
+              }}
+              render={({field: {onChange}}) => (
+                <Pressable
+                  onPress={async () => {
+                    const image = await handleSelectPhoto('openPicker');
+                    console.log(image);
+                    onChange(image);
+                  }}
+                  style={tw`border border-grey2 h-50  items-center justify-center rounded-md`}>
+                  {selectedImage ? (
+                    <CustomImage
+                      resizeMode="contain"
+                      uri={selectedImage}
+                      style={tw`h-50 w-90 rounded-md`}
+                    />
+                  ) : (
+                    <RowContainer>
+                      <GalleryThumbnailSvg />
+                      <CustomText style={tw`ml-3`}>Add an image</CustomText>
+                    </RowContainer>
+                  )}
+                </Pressable>
+              )}
+              name="albumPicture"
+            />
+          </View>
+          <View style={tw`mt-3`}>
+            <ErrorText>{errors?.songs?.message}</ErrorText>
+            <Controller
+              control={control}
+              rules={{
+                required: 'Select a song',
+              }}
+              render={({field: {onChange}}) => (
+                <>
+                  <FormSelector
+                    description="Add music file"
+                    instruction="(max 500mb)"
+                    icon="library-music"
+                    onPress={() => {
+                      const data = handleSelectMusicFile();
+                      const items = data.then(res =>
+                        res?.map(item => ({
+                          name: item.name,
+                          file_url: item.uri,
+                        })),
+                      );
+                      onChange(items);
+                    }}
+                  />
+                  {selectedSongs?.map((item, _) => {
+                    return (
+                      <CustomText
+                        style={tw`text-2xs mt-1 text-purple`}
+                        key={item.name + item?.uri}>
+                        {truncateText(item.name!)}
+                      </CustomText>
+                    );
+                  })}
+                </>
+              )}
+              name="songs"
+            />
+          </View>
+          <View style={tw``}>
+            <ErrorText>{errors?.date?.message}</ErrorText>
+            <Controller
+              control={control}
+              rules={{
+                required: 'Date is required',
+              }}
+              render={({field: {value}}) => (
+                <FormSelector
+                  description="Fix a date"
+                  instruction="Today"
+                  icon="calendar-month"
+                  onPress={() => {
+                    setIsCalendarVisible(true);
+                  }}
+                  value={value}
+                />
+              )}
+              name="date"
+            />
+          </View>
+
+          <VotingPoll />
+          <View style={tw`mt-4`}>
+            <Controller
+              control={control}
+              rules={{
+                required: 'Date is required',
+              }}
+              render={({field: {onChange}}) => (
+                <SwitchSelector
+                  description="Public"
+                  optional
+                  onValueChange={state =>
+                    onChange(state ? 'public' : 'private')
+                  }
+                />
+              )}
+              name="visibility"
+            />
+          </View>
+          <View style={tw`mt-8`}>
+            <TouchableOpacity
+              onPress={handleSubmit(onSubmit)}
+              style={tw`bg-purple h-13 items-center justify-center rounded-full`}>
+              <CustomText style={tw`text-base`}>Save</CustomText>
+            </TouchableOpacity>
+          </View>
           <Controller
             control={control}
             rules={{
               required: 'Date is required',
             }}
-            render={({field: {value}}) => (
-              <FormSelector
-                description="Fix a date"
-                instruction="Today"
-                icon="calendar-month"
-                onPress={() => {
-                  setIsCalendarVisible(true);
-                }}
-                value={value}
+            render={({field: {onChange}}) => (
+              <CustomCalendar
+                isCalendarVisible={isCalendarVisible}
+                onBackDropPress={() => setIsCalendarVisible(false)}
+                onDateSelected={date => onChange(date)}
               />
             )}
             name="date"
           />
         </View>
-
-        <VotingPoll />
-        <View style={tw`mt-4`}>
-          <Controller
-            control={control}
-            rules={{
-              required: 'Date is required',
-            }}
-            render={({field: {onChange}}) => (
-              <SwitchSelector
-                description="Public"
-                optional
-                onValueChange={state => onChange(state ? 'public' : 'private')}
-              />
-            )}
-            name="visibility"
-          />
-        </View>
-        <View style={tw`mt-8`}>
-          <TouchableOpacity
-            onPress={handleSubmit(onSubmit)}
-            style={tw`bg-purple h-13 items-center justify-center rounded-full`}>
-            <CustomText style={tw`text-base`}>Save</CustomText>
-          </TouchableOpacity>
-        </View>
-        <Controller
-          control={control}
-          rules={{
-            required: 'Date is required',
-          }}
-          render={({field: {onChange}}) => (
-            <CustomCalendar
-              isCalendarVisible={isCalendarVisible}
-              onBackDropPress={() => setIsCalendarVisible(false)}
-              onDateSelected={date => onChange(date)}
-            />
-          )}
-          name="date"
-        />
-      </ScrollView>
+      </KeyboardAwareScrollView>
     </ScreenContainer>
   );
 };
