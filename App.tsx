bb1ea50740ca402146fe839cb3c308ae6bc75321
marvelous-ikechi <marvelike7@gmail.com>
2024-04-18 16:04:30 -0700

    installed and setup tanstack

--- App.tsx
+++ App.tsx
@@ -6,13 +6,25 @@ import {View} from 'react-native';
 import tw from 'lib/tailwind';
 import Main from 'app/Main';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
+import {
+  useQuery,
+  useMutation,
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
+
+// Create a client
+const queryClient = new QueryClient();
 
 const App: FunctionComponent = () => {
   return (
     <GestureHandlerRootView style={tw`flex-1`}>
-      <View style={tw`flex-1`}>
-        <Main />
-      </View>
+      <QueryClientProvider client={queryClient}>
+        <View style={tw`flex-1`}>
+          <Main />
+        </View>
+      </QueryClientProvider>
     </GestureHandlerRootView>
   );
 };
