ffbdacc45cae86b0da62d56bce13174a09bb37e0
marvelous-ikechi <marvelike7@gmail.com>
2024-04-19 14:20:09 -0700

    PartyScreen created

--- src/app/navigator/types/MainStackParamList.ts
+++ src/app/navigator/types/MainStackParamList.ts
@@ -1,10 +1,13 @@
 import {NavigatorScreenParams} from '@react-navigation/native';
 import {BottomTabParamList} from './BottomTabParamList';
+import {PartiesResponse} from 'src/types/partyTypes';
 
 export type MainStackParamList = {
   Notifications: undefined;
   CreateNewPost: undefined;
   PlanYourParty: undefined;
-  PartyScreen: undefined;
+  PartyScreen: {
+    party: PartiesResponse;
+  };
   BottomNavigator: NavigatorScreenParams<BottomTabParamList>;
 };
