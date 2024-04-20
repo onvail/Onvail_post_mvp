e649b19df8afae28317d88ca7310e26531140912
marvelous-ikechi <marvelike7@gmail.com>
2024-04-19 20:33:19 -0700

    added handleJoingBtn function

--- src/app/components/Posts/PostCard.tsx
+++ src/app/components/Posts/PostCard.tsx
@@ -1,5 +1,5 @@
-import React, {FunctionComponent, useEffect, useState} from 'react';
-import {Pressable, TouchableOpacity, View} from 'react-native';
+import React, {FunctionComponent} from 'react';
+import {TouchableOpacity, View} from 'react-native';
 import UserHeader from './UserHeader';
 import CustomImage from 'components/Image/CustomImage';
 import tw from 'src/lib/tailwind';
@@ -11,11 +11,12 @@ import MiniMusicPlayer from './MiniMusicPlayer';
 import {PartiesResponse} from 'src/types/partyTypes';
 import Icon from '../Icons/Icon';
 import api from 'src/api/api';
-import useUser, {User} from 'src/app/hooks/useUserInfo';
+import useUser from 'src/app/hooks/useUserInfo';
 import {useMutation, useQueryClient} from '@tanstack/react-query';
 
 interface JoinPartyProps {
-  handleJoinPartyBtnPress: () => void;
+  handleJoinPartyBtnPress: (party: PartiesResponse) => void;
+  party: PartiesResponse;
 }
 
 /**
@@ -25,10 +26,11 @@ interface JoinPartyProps {
  */
 const JoinPartyButton: FunctionComponent<JoinPartyProps> = ({
   handleJoinPartyBtnPress,
+  party,
 }) => {
   return (
     <TouchableOpacity
-      onPress={handleJoinPartyBtnPress}
+      onPress={() => handleJoinPartyBtnPress(party)}
       style={tw`bg-white py-1.7 px-3 rounded-full`}>
       <CustomText style={tw`text-primary text-xs font-poppinsMedium`}>
         Join the party
@@ -45,13 +47,10 @@ const JoinPartyButton: FunctionComponent<JoinPartyProps> = ({
 const PostItem: FunctionComponent<{
   item: PartiesResponse;
   userId: string;
-  handleJoinPartyBtnPress: () => void;
+  handleJoinPartyBtnPress: (item: PartiesResponse) => void;
 }> = ({item, handleJoinPartyBtnPress, userId}) => {
-  const HeartSvg = generalIcon.Heart;
   const CommentSvg = generalIcon.Comment;
   const queryClient = useQueryClient();
-
-  const [like, setLike] = useState<boolean>(false);
 
   const handleFollowMutation = useMutation({
     mutationFn: () => {
@@ -60,7 +59,7 @@ const PostItem: FunctionComponent<{
           url: 'users/unFollowUser',
           authorization: true,
           data: {
-            userIdTounFollow: item?.artist?._id,
+            userIdToUnfollow: item?.artist?._id,
           },
         });
       } else {
@@ -126,7 +125,7 @@ const PostItem: FunctionComponent<{
           <RowContainer>
             <CommentSvg />
             <CustomText style={tw`ml-2 text-grey2`}>
-              {item?.likes?.length ?? 0}
+              {item?.comments?.length ?? 0}
             </CustomText>
           </RowContainer>
         </RowContainer>
@@ -139,6 +138,10 @@ const PostItem: FunctionComponent<{
         ) : (
           <JoinPartyButton handleJoinPartyBtnPress={handleJoinPartyBtnPress} />
         )} */}
+        <JoinPartyButton
+          party={item}
+          handleJoinPartyBtnPress={() => handleJoinPartyBtnPress(item)}
+        />
       </RowContainer>
     </View>
   );
@@ -151,7 +154,7 @@ const PostItem: FunctionComponent<{
  */
 
 interface PostCardProps {
-  handleJoinPartyBtnPress: () => void;
+  handleJoinPartyBtnPress: (item: PartiesResponse) => void;
   data: PartiesResponse[];
 }
 const PostCard: FunctionComponent<PostCardProps> = ({
@@ -167,12 +170,19 @@ const PostCard: FunctionComponent<PostCardProps> = ({
       userId={user?._id ?? ''}
     />
   );
+
+  // NOTE
+  // The custom navbar gets a background color placed behind the Onvail button when a margin-bottom is used to push the last-item to a visible position.
+  // Adding a Footer component adds an extra item that makes the last component appear within users view, which in-turn helps for interaction.
+  const renderFooterComponent = () => <View style={tw`h-12`} />;
+
   return (
     <FlashList
       data={data}
       renderItem={renderItem}
       estimatedItemSize={15}
       showsHorizontalScrollIndicator={false}
+      ListFooterComponent={renderFooterComponent}
     />
   );
 };
