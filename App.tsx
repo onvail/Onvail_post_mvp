22b7afb3ad145f4dfc58e896fe7c3a36695e824f
marvelous-ikechi <marvelike7@gmail.com>
2024-04-25 10:36:55 -0700

    removed imports

--- App.tsx
+++ App.tsx
@@ -7,14 +7,11 @@ import tw from 'lib/tailwind';
 import Main from 'app/Main';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
-import {Cloudinary} from '@cloudinary/url-gen';
 
 // Create a Query client
 const queryClient = new QueryClient();
 
 const App: FunctionComponent = () => {
-  const _cld = new Cloudinary({cloud: {cloudName: 'dycpdxcii'}});
-
   return (
     <GestureHandlerRootView style={tw`flex-1`}>
       <QueryClientProvider client={queryClient}>
