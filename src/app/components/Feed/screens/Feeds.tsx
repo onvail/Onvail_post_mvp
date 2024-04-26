ce3e01a624e5201e8091a9b8cea882821b022c34
marvelous-ikechi <marvelike7@gmail.com>
2024-04-26 14:18:30 -0700

    added like and comment sections

--- src/app/components/Feed/screens/Feeds.tsx
+++ src/app/components/Feed/screens/Feeds.tsx
@@ -5,11 +5,14 @@ import {FlashList, ListRenderItem} from '@shopify/flash-list';
 import {FeedResponse} from 'src/types/partyTypes';
 import tw from 'src/lib/tailwind';
 import useUser from 'src/app/hooks/useUserInfo';
-import {classifyUrl} from 'src/utils/utilities';
+import {classifyUrl, truncateText} from 'src/utils/utilities';
 import CustomImage from 'app/components/Image/CustomImage';
 import MiniMusicPlayer from '../../Posts/MiniMusicPlayer';
 import uuid from 'react-native-uuid';
 import CustomText from '../../Text/CustomText';
+import RowContainer from '../../View/RowContainer';
+import {generalIcon} from '../../Icons/generalIcons';
+import Icon from '../../Icons/Icon';
 
 interface FeedItemProps {
   item: FeedResponse;
@@ -19,7 +22,9 @@ interface FeedItemProps {
 const FeedItem: FunctionComponent<FeedItemProps> = ({item, userId}) => {
   const {user} = item;
   const canFollowUser = item?.user?.profile?._id !== userId;
-  console.log(item);
+  const isLiked = item?.likes?.some(likes => likes?._id === userId);
+
+  const CommentSvg = generalIcon.Comment;
 
   return (
     <View style={tw`mb-4 flex-1`}>
@@ -30,8 +35,10 @@ const FeedItem: FunctionComponent<FeedItemProps> = ({item, userId}) => {
         canFollow={canFollowUser}
         isFollowing={false}
       />
-      <CustomText style={tw`mt-2 text-xs mx-3`}>{item?.text}</CustomText>
-      <View style={tw`mt-2 h-100`}>
+      <CustomText style={tw`mt-2 text-xs mx-3`}>
+        {truncateText(item?.text, 120)}
+      </CustomText>
+      <View style={tw` mt-3`}>
         {item?.mediaFiles?.map((mediaFile, _) => {
           const urlType = classifyUrl(mediaFile);
           if (urlType.type === 'Image') {
@@ -39,19 +46,41 @@ const FeedItem: FunctionComponent<FeedItemProps> = ({item, userId}) => {
               <CustomImage
                 uri={urlType.url}
                 key={mediaFile}
+                resizeMode="stretch"
                 style={tw`h-100 w-100`}
               />
             );
           } else if (urlType.type === 'Music') {
             return (
-              <View style={tw`mb-1 mx-3`} key={mediaFile}>
+              <RowContainer
+                style={tw`mb-1 mx-3 justify-between`}
+                key={mediaFile}>
+                <RowContainer>
+                  <RowContainer style={tw`mr-5`}>
+                    <Icon
+                      icon={isLiked ? 'heart' : 'heart-outline'}
+                      color={isLiked ? 'red' : 'white'}
+                      onPress={() => {}}
+                      size={20}
+                    />
+                    <CustomText style={tw`ml-2 text-grey2`}>
+                      {item?.likes?.length ?? 0}
+                    </CustomText>
+                  </RowContainer>
+                  <RowContainer>
+                    <CommentSvg />
+                    <CustomText style={tw`ml-2 text-grey2`}>
+                      {item?.comments?.length ?? 0}
+                    </CustomText>
+                  </RowContainer>
+                </RowContainer>
                 <MiniMusicPlayer
                   uri={urlType.url}
                   artiste={user?.name}
                   musicTitle={'Dandizzy'}
                   id={uuid.v4().toString()}
                 />
-              </View>
+              </RowContainer>
             );
           } else {
             return null;
@@ -73,10 +102,11 @@ const Feeds: FunctionComponent<FeedsProps> = ({data}) => {
   );
 
   const renderFooterComponent = () => <View style={tw`h-12`} />;
+  const reversedItems = [...data].reverse();
 
   return (
     <FlashList
-      data={data}
+      data={reversedItems}
       renderItem={renderItem}
       estimatedItemSize={15}
       showsHorizontalScrollIndicator={false}
