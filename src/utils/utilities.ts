7150fb492c0a3e7453049a0459c70dec1f54e73b
marvelous-ikechi <marvelike7@gmail.com>
2024-04-26 13:35:49 -0700

    deploy files to folders

--- src/utils/utilities.ts
+++ src/utils/utilities.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import {CLOUD_NAME} from '@env';
+import {CLOUD_NAME, UPLOAD_PRESET} from '@env';
 
 export const truncateText = (text: string, maxLength: number = 40): string => {
   return (
@@ -76,19 +76,40 @@ export const uploadToCloudinary = async (item: FileUploadItem) => {
     type: item.type,
     name: item.name,
   });
-  data.append('upload_preset', 'xk7fxbhv');
+  data.append('upload_preset', UPLOAD_PRESET);
   data.append('cloud_name', CLOUD_NAME);
+
+  // Determine the folder based on MIME type
+  let resourceType = 'auto'; // Default is auto
+  let folder = 'other';
+  if (item.type.startsWith('image/')) {
+    folder = 'images';
+    resourceType = 'image';
+  } else if (item.type.startsWith('video/')) {
+    folder = 'videos';
+    resourceType = 'video';
+  } else if (item.type.startsWith('audio/')) {
+    folder = 'music';
+    resourceType = 'video'; // Consider changing to 'audio' if no video stream is intended
+  }
+
+  data.append('folder', folder);
+  data.append('resource_type', resourceType);
+
+  // Append the folder to FormData
+  data.append('folder', folder);
+
   try {
     const response = await axios.post(
       `https://api.cloudinary.com/v1_1/${CLOUD_NAME}/upload`,
       data,
     );
     return {
-      name: response?.data?.original_filename,
-      file_url: response?.data?.secure_url,
+      name: response.data.original_filename,
+      file_url: response.data.secure_url,
     };
   } catch (error) {
-    console.log(error);
+    console.error('Upload Error:', error);
   }
 };
 
@@ -110,5 +131,21 @@ export const handleMultipleUploads = async (files: FileUploadItem[]) => {
     return uploadResults;
   } catch (error) {
     console.error('Error in handling multiple uploads:', error);
+  }
+};
+
+export const classifyUrl = (
+  url: string,
+): {url: string; type: 'Image' | 'Video' | 'Music' | 'Unknown type'} => {
+  if (url?.includes('/image/upload/')) {
+    return {url, type: 'Image'};
+  } else if (url?.includes('/music/')) {
+    console.log('music', url);
+    return {url, type: 'Music'};
+  } else if (url?.includes('/video/upload/')) {
+    console.log('video', url);
+    return {url, type: 'Video'};
+  } else {
+    return {url, type: 'Unknown type'};
   }
 };
