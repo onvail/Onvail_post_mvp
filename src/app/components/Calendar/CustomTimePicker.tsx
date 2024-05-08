51d994fa874b83ae930a44fe3313c86a19cc963d
marvelous-ikechi <marvelike7@gmail.com>
2024-05-08 18:16:15 +0100

    update Date picker component

--- src/app/components/Calendar/CustomTimePicker.tsx
+++ src/app/components/Calendar/CustomTimePicker.tsx
@@ -1,53 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {SafeAreaView} from 'react-native';
-import {
-  CustomTimerPickerStyles,
-  TimerPickerModal,
-} from 'react-native-timer-picker';
-import LinearGradient from 'react-native-linear-gradient';
+import DateTimePicker from '@react-native-community/datetimepicker';
 
 interface Props {
   showTimePicker: boolean;
-  onChangeTime: (timeObj: {
-    hours: number;
-    minutes: number;
-    seconds: number;
-  }) => void;
-  handleVisibility: () => void;
-  onCancel: () => void;
+  onChangeTime: (time: string | undefined) => void;
 }
 
-const CustomDateTimePicker: React.FC<Props> = ({
-  showTimePicker,
-  onChangeTime,
-  handleVisibility,
-  onCancel,
-}) => {
-  const style: CustomTimerPickerStyles = {theme: 'dark'};
+const CustomTimePicker: React.FC<Props> = ({showTimePicker, onChangeTime}) => {
+  const [date, setDate] = useState<any>(new Date(1598051730000));
   return (
     <SafeAreaView>
       {showTimePicker && (
-        <TimerPickerModal
-          visible={showTimePicker}
-          setIsVisible={handleVisibility}
-          onConfirm={pickedDuration => {
-            onChangeTime(pickedDuration);
+        <DateTimePicker
+          testID="dateTimePicker"
+          value={date}
+          mode={'time'}
+          onChange={(_, selectedDate) => {
+            onChangeTime(selectedDate?.toLocaleTimeString());
+            setDate(selectedDate);
           }}
-          hourLabel=":"
-          minuteLabel=":"
-          secondLabel=""
-          modalTitle="Set Time"
-          onCancel={onCancel}
-          closeOnOverlayPress
-          LinearGradient={LinearGradient}
-          styles={style}
-          modalProps={{
-            overlayOpacity: 0.2,
-          }}
+          display="spinner"
         />
       )}
     </SafeAreaView>
   );
 };
 
-export default CustomDateTimePicker;
+export default CustomTimePicker;
