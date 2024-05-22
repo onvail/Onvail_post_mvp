55e6471ca4370ecd347fe5570890b8b3429191fb
marvelous-ikechi <marvelike7@gmail.com>
2024-05-22 12:54:37 +0100

    role input screen completely cleanedup

--- src/app/auth/screens/RoleInput.tsx
+++ src/app/auth/screens/RoleInput.tsx
@@ -1,30 +1,26 @@
-import React, {FunctionComponent, useEffect, useState} from 'react';
-import {ImageBackground, TouchableOpacity, View} from 'react-native';
+import React, {FunctionComponent} from 'react';
+import {TouchableOpacity, View} from 'react-native';
 import {useForm, Controller} from 'react-hook-form';
 import CustomText from 'app/components/Text/CustomText';
 import tw from 'lib/tailwind';
-import CustomTextInput from 'app/components/TextInput/CustomTextInput';
-import ProceedBtn from 'src/app/components/Buttons/ProceedBtn';
-import CustomDropDown from 'src/app/components/Dropdown/CustomDropDown';
-import {roles} from 'src/utils/roles';
 import {AuthStackParamList} from 'src/app/navigator/types/AuthStackParamList';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
-import {generalIcon} from 'src/app/components/Icons/generalIcons';
-import {AuthError, SignInProps, UserType} from 'src/types/authType';
 import ErrorText from 'src/app/components/Text/ErrorText';
-import api from 'src/api/api';
-import messaging from '@react-native-firebase/messaging';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import localStorageKeys from 'src/api/config/local-storage-keys';
-import ScreenContainer from 'src/app/components/Screens/ScreenContainer';
 import RowContainer from 'src/app/components/View/RowContainer';
 import Icon from 'src/app/components/Icons/Icon';
+import AuthScreenContainer from 'src/app/components/Screens/AuthScreenContainer';
+import CustomDropDown from 'src/app/components/Dropdown/CustomDropDown';
+import {roles} from 'src/utils/roles';
+import {UserType} from 'src/types/authType';
+import {SignUpStoreState, useSignUpStore} from 'src/app/zustand/store';
 
 type Props = NativeStackScreenProps<AuthStackParamList, 'Signup'>;
 
 const RoleInput: FunctionComponent<Props> = ({navigation}) => {
-  const defaultValues = {
-    role: '',
+  const defaultValues: {
+    userType: UserType;
+  } = {
+    userType: UserType.Other,
   };
 
   const {
@@ -36,11 +32,21 @@ const RoleInput: FunctionComponent<Props> = ({navigation}) => {
     mode: 'all',
   });
 
+  const updateUserSignUpStore = useSignUpStore(
+    (state: SignUpStoreState) => state.updateUserSignUpStore,
+  );
+  const storeUser = useSignUpStore((state: SignUpStoreState) => state.user);
+
+  const onSubmit = async ({userType}: {userType: UserType}) => {
+    updateUserSignUpStore({
+      ...storeUser,
+      userType,
+    });
+    navigation.navigate('FullNameInput');
+  };
+
   return (
-    <ImageBackground
-      source={require('../../../assets/passwordbg.png')}
-      resizeMode="cover"
-      style={tw`w-full h-full`}>
+    <AuthScreenContainer>
       <View style={tw`mt-12 px-3`}>
         <RowContainer style={tw`mb-12 mt-3 items-center`}>
           <Icon
@@ -51,37 +57,41 @@ const RoleInput: FunctionComponent<Props> = ({navigation}) => {
           />
           <CustomText style={tw`text-lg ml-24`}>Create account</CustomText>
         </RowContainer>
-        <CustomText style={tw`text-white font-bold text-lg relative top-4`}>
+        <CustomText style={tw`text-white font-bold text-lg`}>
           What’s your role in the music industry?
         </CustomText>
         <View>
-          <ErrorText>{errors?.role?.message}</ErrorText>
+          <ErrorText>{errors?.userType?.message}</ErrorText>
           <Controller
             control={control}
             rules={{
-              required: 'Role is required',
+              required: 'select your role',
             }}
             render={({field: {onChange, onBlur, value}}) => (
-              <CustomTextInput
-                placeholder="Role"
-                onChangeText={onChange}
+              <CustomDropDown
+                data={roles}
+                onChange={item => onChange(item.value)}
+                labelField={'key'}
+                valueField={'value'}
+                maxHeight={250}
                 value={value}
                 onBlur={onBlur}
+                placeholder="Select user type"
               />
             )}
-            name="role"
+            name="userType"
           />
         </View>
       </View>
       <TouchableOpacity
         activeOpacity={0.8}
-        onPress={() => navigation.navigate('FullNameInput')}
+        onPress={handleSubmit(onSubmit)}
         style={tw`w-1/4 rounded-12 bg-[#7C1AFC] self-center mt-12 py-4.5`}>
         <CustomText style={tw`text-white text-center font-bold text-lg`}>
           Next
         </CustomText>
       </TouchableOpacity>
-    </ImageBackground>
+    </AuthScreenContainer>
   );
 };
 
