08e5b169c8b172981cbc03134c56a7b97ba79055
marvelous-ikechi <marvelike7@gmail.com>
2024-05-22 10:59:38 +0100

    code cleanup

--- src/app/auth/screens/EmailScreen.tsx
+++ src/app/auth/screens/EmailScreen.tsx
@@ -4,32 +4,19 @@ import {useForm, Controller} from 'react-hook-form';
 import CustomText from 'app/components/Text/CustomText';
 import tw from 'lib/tailwind';
 import CustomTextInput from 'app/components/TextInput/CustomTextInput';
-import ProceedBtn from 'src/app/components/Buttons/ProceedBtn';
-import CustomDropDown from 'src/app/components/Dropdown/CustomDropDown';
-import {roles} from 'src/utils/roles';
 import {AuthStackParamList} from 'src/app/navigator/types/AuthStackParamList';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
-import {generalIcon} from 'src/app/components/Icons/generalIcons';
-import {AuthError, SignInProps, UserType} from 'src/types/authType';
 import ErrorText from 'src/app/components/Text/ErrorText';
-import api from 'src/api/api';
 import messaging from '@react-native-firebase/messaging';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import localStorageKeys from 'src/api/config/local-storage-keys';
-import ScreenContainer from 'src/app/components/Screens/ScreenContainer';
 import RowContainer from 'src/app/components/View/RowContainer';
 import Icon from 'src/app/components/Icons/Icon';
 
 type Props = NativeStackScreenProps<AuthStackParamList, 'Signup'>;
 
 const EmailInput: FunctionComponent<Props> = ({navigation}) => {
-  const EmailBackgroundGradientSvg = generalIcon.EmailBackgroundGradient;
-  const [isPasswordHidden, setIsPasswordHidden] = useState<boolean>(true);
   const [apnToken, setApnToken] = useState<string>('');
-  const [signUpError, setSignUpError] = useState<string | undefined>(undefined);
-  const [isLoading, setIsLoading] = useState<boolean>(false);
 
-  const defaultValues: SignInProps = {
+  const defaultValues: {email: string} = {
     email: '',
   };
 
@@ -41,31 +28,8 @@ const EmailInput: FunctionComponent<Props> = ({navigation}) => {
     defaultValues: defaultValues,
     mode: 'all',
   });
-  const onSubmit = async (data: SignInProps) => {
-    setIsLoading(true);
-    setSignUpError(undefined);
-    const formData = {
-      ...data,
-      FCMToken: apnToken,
-    };
-    try {
-      const response = await api.post({
-        url: 'users/register',
-        data: formData,
-      });
-      AsyncStorage.multiSet([
-        [localStorageKeys.accessToken, response?.data?.accessToken],
-        [localStorageKeys.userInfo, JSON.stringify(response?.data)],
-      ]);
-      navigation.navigate('BottomTabNavigator', {
-        screen: 'Home',
-      });
-    } catch (err: unknown) {
-      const error = err as AuthError;
-      console.log(error.response?.data);
-      setSignUpError(error?.response?.data?.message);
-    }
-    setIsLoading(false);
+  const onSubmit = async () => {
+    navigation.navigate('PasswordInput');
   };
 
   useEffect(() => {
@@ -102,7 +66,7 @@ const EmailInput: FunctionComponent<Props> = ({navigation}) => {
           />
           <CustomText style={tw`text-lg ml-24`}>Create account</CustomText>
         </RowContainer>
-        <CustomText style={tw`text-white font-bold text-lg relative top-4`}>
+        <CustomText style={tw`text-white font-bold text-lg relative`}>
           What’s your email?
         </CustomText>
         <View>
@@ -133,7 +97,7 @@ const EmailInput: FunctionComponent<Props> = ({navigation}) => {
       </View>
       <TouchableOpacity
         activeOpacity={0.8}
-        onPress={() => navigation.navigate('PasswordInput')}
+        onPress={handleSubmit(onSubmit)}
         style={tw`w-1/4 rounded-12 bg-[#7C1AFC] self-center mt-5 py-4.5`}>
         <CustomText style={tw`text-white text-center font-bold text-lg`}>
           Next
