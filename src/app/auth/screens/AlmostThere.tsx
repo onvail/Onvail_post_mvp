94cb97b32b6727321f09be1fb222e219a8b4ff2a
marvelous-ikechi <marvelike7@gmail.com>
2024-05-22 12:26:35 +0100

    code cleanup

--- src/app/auth/screens/AlmostThere.tsx
+++ src/app/auth/screens/AlmostThere.tsx
@@ -1,48 +1,17 @@
-import React, {FunctionComponent, useEffect, useState} from 'react';
+import React, {FunctionComponent} from 'react';
 import {ImageBackground, TouchableOpacity, View} from 'react-native';
-import {useForm, Controller} from 'react-hook-form';
 import CustomText from 'app/components/Text/CustomText';
 import tw from 'lib/tailwind';
-import CustomTextInput from 'app/components/TextInput/CustomTextInput';
-import ProceedBtn from 'src/app/components/Buttons/ProceedBtn';
-import CustomDropDown from 'src/app/components/Dropdown/CustomDropDown';
-import {roles} from 'src/utils/roles';
+
 import {AuthStackParamList} from 'src/app/navigator/types/AuthStackParamList';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
-import {generalIcon} from 'src/app/components/Icons/generalIcons';
-import {AuthError, SignInProps, UserType} from 'src/types/authType';
-import ErrorText from 'src/app/components/Text/ErrorText';
-import api from 'src/api/api';
-import messaging from '@react-native-firebase/messaging';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import localStorageKeys from 'src/api/config/local-storage-keys';
-import ScreenContainer from 'src/app/components/Screens/ScreenContainer';
+
 import RowContainer from 'src/app/components/View/RowContainer';
 import Icon from 'src/app/components/Icons/Icon';
-import {genderOptions} from 'src/utils/gender';
 
 type Props = NativeStackScreenProps<AuthStackParamList, 'Signup'>;
 
 const AlmostThere: FunctionComponent<Props> = ({navigation}) => {
-  const EmailBackgroundGradientSvg = generalIcon.EmailBackgroundGradient;
-  const [isPasswordHidden, setIsPasswordHidden] = useState<boolean>(true);
-  const [apnToken, setApnToken] = useState<string>('');
-  const [signUpError, setSignUpError] = useState<string | undefined>(undefined);
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-
-  const defaultValues = {
-    gender: '',
-  };
-
-  const {
-    control,
-    handleSubmit,
-    formState: {errors},
-  } = useForm({
-    defaultValues: defaultValues,
-    mode: 'all',
-  });
-
   return (
     <ImageBackground
       source={require('../../../assets/passwordbg.png')}
