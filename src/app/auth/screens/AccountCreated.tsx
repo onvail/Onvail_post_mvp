7108c870241821f11937d2e43cc97889570cfbf9
marvelous-ikechi <marvelike7@gmail.com>
2024-05-22 13:26:42 +0100

    updated styling

--- src/app/auth/screens/AccountCreated.tsx
+++ src/app/auth/screens/AccountCreated.tsx
@@ -1,44 +1,26 @@
 import React, {FunctionComponent} from 'react';
-import {
-  ImageBackground,
-  ScrollView,
-  TouchableOpacity,
-  View,
-} from 'react-native';
+import {ScrollView, TouchableOpacity, View} from 'react-native';
 import CustomText from 'app/components/Text/CustomText';
 import tw from 'lib/tailwind';
 import {AuthStackParamList} from 'src/app/navigator/types/AuthStackParamList';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
-import RowContainer from 'src/app/components/View/RowContainer';
-import Icon from 'src/app/components/Icons/Icon';
+import AuthScreenContainer from 'src/app/components/Screens/AuthScreenContainer';
 
 type Props = NativeStackScreenProps<AuthStackParamList, 'Signup'>;
 
 const AccountCreated: FunctionComponent<Props> = ({navigation}) => {
   return (
-    <ImageBackground
-      source={require('../../../assets/toubg.png')}
-      resizeMode="cover"
-      style={tw`w-full h-full`}>
+    <AuthScreenContainer>
       <View>
         <ScrollView showsVerticalScrollIndicator={false}>
           <View style={tw`mt-12 px-3`}>
-            <RowContainer style={tw`mb-12 mt-3 items-center`}>
-              <Icon
-                onPress={() => navigation.goBack()}
-                icon={'chevron-left'}
-                color="white"
-                size={30}
-              />
-              <CustomText style={tw`text-lg ml-24`}>Create account</CustomText>
-            </RowContainer>
             <View style={tw`mt-32`}>
               <CustomText
                 style={tw`text-2xl font-bold text-white text-center mx-auto mb-2`}>
                 All done!
               </CustomText>
               <CustomText
-                style={tw`text-lg font-bold text-white text-center mx-auto`}>
+                style={tw`text-lg font-poppinsRegular text-white text-center mx-auto`}>
                 Thank you for verifying your email address.
               </CustomText>
             </View>
@@ -57,7 +39,7 @@ const AccountCreated: FunctionComponent<Props> = ({navigation}) => {
           </TouchableOpacity>
         </ScrollView>
       </View>
-    </ImageBackground>
+    </AuthScreenContainer>
   );
 };
 
