e4a7d2b44f0e8e9681a43a6886109f650c2e38f8
marvelous-ikechi <marvelike7@gmail.com>
2024-05-22 12:28:58 +0100

    - code cleanup - saved data in global store

--- src/app/auth/screens/GenderInput.tsx
+++ src/app/auth/screens/GenderInput.tsx
@@ -1,22 +1,14 @@
-import React, {FunctionComponent, useEffect, useState} from 'react';
+import React, {FunctionComponent} from 'react';
 import {ImageBackground, TouchableOpacity, View} from 'react-native';
 import {useForm, Controller} from 'react-hook-form';
 import CustomText from 'app/components/Text/CustomText';
 import tw from 'lib/tailwind';
-import CustomTextInput from 'app/components/TextInput/CustomTextInput';
-import ProceedBtn from 'src/app/components/Buttons/ProceedBtn';
 import CustomDropDown from 'src/app/components/Dropdown/CustomDropDown';
-import {roles} from 'src/utils/roles';
 import {AuthStackParamList} from 'src/app/navigator/types/AuthStackParamList';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
-import {generalIcon} from 'src/app/components/Icons/generalIcons';
-import {AuthError, SignInProps, UserType} from 'src/types/authType';
 import ErrorText from 'src/app/components/Text/ErrorText';
-import api from 'src/api/api';
-import messaging from '@react-native-firebase/messaging';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import localStorageKeys from 'src/api/config/local-storage-keys';
-import ScreenContainer from 'src/app/components/Screens/ScreenContainer';
+import {SignUpStoreState, useSignUpStore} from 'src/app/zustand/store';
+
 import RowContainer from 'src/app/components/View/RowContainer';
 import Icon from 'src/app/components/Icons/Icon';
 import {genderOptions} from 'src/utils/gender';
@@ -24,15 +16,16 @@ import {genderOptions} from 'src/utils/gender';
 type Props = NativeStackScreenProps<AuthStackParamList, 'Signup'>;
 
 const GenderInput: FunctionComponent<Props> = ({navigation}) => {
-  const EmailBackgroundGradientSvg = generalIcon.EmailBackgroundGradient;
-  const [isPasswordHidden, setIsPasswordHidden] = useState<boolean>(true);
-  const [apnToken, setApnToken] = useState<string>('');
-  const [signUpError, setSignUpError] = useState<string | undefined>(undefined);
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-
-  const defaultValues = {
+  const defaultValues: {
+    gender: 'male' | 'female';
+  } = {
     gender: '',
   };
+
+  const updateUserSignUpStore = useSignUpStore(
+    (state: SignUpStoreState) => state.updateUserSignUpStore,
+  );
+  const storeUser = useSignUpStore((state: SignUpStoreState) => state.user);
 
   const {
     control,
@@ -42,6 +35,14 @@ const GenderInput: FunctionComponent<Props> = ({navigation}) => {
     defaultValues: defaultValues,
     mode: 'all',
   });
+
+  const onSubmit = async ({dateOfBirth}: {dateOfBirth: string}) => {
+    updateUserSignUpStore({
+      ...storeUser,
+      dateOfBirth,
+    });
+    navigation.navigate('AlmostThere');
+  };
 
   return (
     <ImageBackground
@@ -86,7 +87,7 @@ const GenderInput: FunctionComponent<Props> = ({navigation}) => {
       </View>
       <TouchableOpacity
         activeOpacity={0.8}
-        onPress={() => navigation.navigate('AlmostThere')}
+        onPress={handleSubmit(onSubmit)}
         style={tw`w-1/4 rounded-12 bg-[#7C1AFC] self-center mt-5 py-4.5`}>
         <CustomText style={tw`text-white text-center font-bold text-lg`}>
           Next
