fd8c69d55f59b60c297091fbd090d542df65b22a
marvelous-ikechi <marvelike7@gmail.com>
2024-05-23 13:14:52 +0100

    style updated

--- src/app/components/Status/Status.tsx
+++ src/app/components/Status/Status.tsx
@@ -1,6 +1,12 @@
 import {FlashList, ListRenderItem} from '@shopify/flash-list';
 import React, {FunctionComponent, memo} from 'react';
-import {View, ImageSourcePropType, Text, TouchableOpacity, ScrollView} from 'react-native';
+import {
+  View,
+  ImageSourcePropType,
+  Text,
+  TouchableOpacity,
+  ScrollView,
+} from 'react-native';
 import tw from 'src/lib/tailwind';
 import {sampleStatus} from 'src/utils/data';
 import CustomImage from 'components/Image/CustomImage';
@@ -17,11 +23,11 @@ const StatusItemComponent: FunctionComponent<{item: StatusItem}> = memo(
 
     return (
       <View style={tw`items-center`}>
-        <View style={tw`mr-3 rounded-[17px] border-4 border-grey5`}>
+        <View style={tw`mr-3 rounded-[17px] border-[0.5] border-grey5`}>
           <CustomImage
             resizeMode="cover"
             uri={imageSource.uri!}
-            style={tw`rounded-[15px] h-19 w-19  border-2 `}
+            style={tw`rounded-[15px] h-[65px] w-[65px]  border-2 `}
           />
         </View>
         <Text style={tw`text-white mt-2 font-poppinsMedium text-xs`}>
@@ -42,13 +48,14 @@ const Status: FunctionComponent = () => {
     <View style={tw`flex-row`}>
       <TouchableOpacity activeOpacity={0.9} style={tw`items-center`}>
         <View
-          style={tw`mr-3 items-center justify-center rounded-[17px] h-20 w-20 border-4 border-grey5`}>
+          style={tw`mr-3 items-center justify-center rounded-[17px]  border-2 border-dotted border-grey5`}>
           <View
-            style={tw`items-center justify-center rounded-[17px] bg-[#7C1AFC] h-19 w-19 border-2`}>
+            style={tw`items-center justify-center rounded-[17px] bg-[#7C1AFC] h-[65px] w-[65px] border-2`}>
             <AddIcon />
           </View>
         </View>
-        <Text style={tw`text-white mt-2 font-poppinsMedium text-xs`}>
+        <Text
+          style={tw`text-white mt-2 text-center font-poppinsMedium text-xs`}>
           Your Story
         </Text>
       </TouchableOpacity>
@@ -56,7 +63,7 @@ const Status: FunctionComponent = () => {
         <FlashList
           data={sampleStatus}
           renderItem={renderItem}
-          estimatedItemSize={10}
+          estimatedItemSize={100}
           horizontal
           keyExtractor={item => item.name}
           showsHorizontalScrollIndicator={false}
