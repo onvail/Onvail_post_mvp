90a55d356d0643770ae03b69e2fbd2e3669d7977
marvelous-ikechi <marvelike7@gmail.com>
2024-05-29 11:09:12 +0100

    pased data to comments

--- src/app/components/Cards/CommentCards.tsx
+++ src/app/components/Cards/CommentCards.tsx
@@ -1,23 +1,30 @@
 import React, {FunctionComponent, useState} from 'react';
-import {generalIcon} from '../Icons/generalIcons';
 import CustomText from '../Text/CustomText';
 import tw from 'src/lib/tailwind';
 import RowContainer from '../View/RowContainer';
 import Icon from '../Icons/Icon';
 import {Colors} from 'src/app/styles/colors';
 import {Pressable, TouchableOpacity, View} from 'react-native';
+import {Comment} from 'src/types/partyTypes';
+import CustomImage from '../Image/CustomImage';
 
-const CommentCards: FunctionComponent = () => {
-  const ProfileImage = generalIcon.ProfileImage;
+const CommentCards: FunctionComponent<{item: Comment}> = ({item}) => {
   const [isMuted, setIsMuted] = useState<boolean>(false);
   const [isLiked, setIsLiked] = useState<boolean>(false);
   return (
     <View style={tw`flex-1 border-grey4 border-b-[0.2px] py-3 px-4`}>
       <RowContainer style={tw`flex-1 flex-row justify-between   `}>
         <RowContainer>
-          <ProfileImage />
+          {item?.user?.image ? (
+            <CustomImage
+              uri={item?.user?.image}
+              style={tw`h-5 w-5 rounded-full`}
+            />
+          ) : (
+            <Icon icon={'account'} color="white" size={20} />
+          )}
           <CustomText style={tw`text-[10px] ml-5 w-[75%]`}>
-            Sleeping with the fishes made the meats run dry in her episconic
+            {item?.text}
           </CustomText>
         </RowContainer>
         <Pressable onPress={() => setIsMuted(!isMuted)}>
@@ -38,7 +45,7 @@ const CommentCards: FunctionComponent = () => {
             size={20}
           />
         </TouchableOpacity>
-        <CustomText style={tw`ml-2 text-xs`}>10</CustomText>
+        <CustomText style={tw`ml-2 text-xs`}>{item?.likes?.length}</CustomText>
       </RowContainer>
     </View>
   );
