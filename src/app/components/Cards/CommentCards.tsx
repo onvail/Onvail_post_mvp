802ee39b18a2039e8a5ff3b461bf8435da2a92ff
marvelous-ikechi <marvelike7@gmail.com>
2024-05-30 10:12:29 +0100

    updated comments  data from firebase

--- src/app/components/Cards/CommentCards.tsx
+++ src/app/components/Cards/CommentCards.tsx
@@ -1,23 +1,51 @@
-import React, {FunctionComponent, useState} from 'react';
+import React, {FunctionComponent, useEffect, useState} from 'react';
 import CustomText from '../Text/CustomText';
 import tw from 'src/lib/tailwind';
 import RowContainer from '../View/RowContainer';
 import Icon from '../Icons/Icon';
 import {Colors} from 'src/app/styles/colors';
 import {Pressable, TouchableOpacity, View} from 'react-native';
-import {Comment} from 'src/types/partyTypes';
 import CustomImage from '../Image/CustomImage';
+import {
+  FireStoreComments,
+  likeComment,
+  unlikeComment,
+} from 'src/actions/parties';
+import useUser from 'src/app/hooks/useUserInfo';
 
-const CommentCards: FunctionComponent<{item: Comment}> = ({item}) => {
+const CommentCards: FunctionComponent<{
+  item: FireStoreComments;
+  partyId: string;
+}> = ({item, partyId}) => {
   const [isMuted, setIsMuted] = useState<boolean>(false);
   const [isLiked, setIsLiked] = useState<boolean>(false);
+  const {user} = useUser();
+
+  useEffect(() => {
+    if (item?.likes?.includes?.(user?._id)) {
+      setIsLiked(true);
+    } else {
+      setIsLiked(false);
+    }
+  }, [item?.likes, user?._id]);
+
+  const handleLike = async () => {
+    if (item?.likes?.includes?.(user?._id)) {
+      await unlikeComment(partyId, item?.commentId, user?._id);
+      setIsLiked(false);
+    } else {
+      await likeComment(partyId, item?.commentId, user?._id);
+      setIsLiked(true);
+    }
+  };
+
   return (
     <View style={tw`flex-1 border-grey4 border-b-[0.2px] py-3 px-4`}>
       <RowContainer style={tw`flex-1 flex-row justify-between   `}>
         <RowContainer>
-          {item?.user?.image ? (
+          {item?.imageUri ? (
             <CustomImage
-              uri={item?.user?.image}
+              uri={item?.imageUri}
               style={tw`h-5 w-5 rounded-full`}
             />
           ) : (
@@ -36,16 +64,16 @@ const CommentCards: FunctionComponent<{item: Comment}> = ({item}) => {
         </Pressable>
       </RowContainer>
       <RowContainer style={tw`mt-4`}>
-        <TouchableOpacity
-          onPress={() => setIsLiked(!isLiked)}
-          style={tw` ml-12`}>
+        <TouchableOpacity onPress={() => handleLike()} style={tw` ml-12`}>
           <Icon
             icon={isLiked ? 'heart' : 'heart-outline'}
             color={Colors.red}
             size={20}
           />
         </TouchableOpacity>
-        <CustomText style={tw`ml-2 text-xs`}>{item?.likes?.length}</CustomText>
+        <CustomText style={tw`ml-2 text-xs`}>
+          {item?.likes?.length ?? 0}
+        </CustomText>
       </RowContainer>
     </View>
   );
