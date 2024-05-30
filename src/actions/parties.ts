0771902eb5e2777d6b9e858031930d54a1388c8d
marvelous-ikechi <marvelike7@gmail.com>
2024-05-30 10:11:31 +0100

    added firebase functions for handling parties

--- src/actions/parties.ts
+++ src/actions/parties.ts
@@ -1,9 +1,32 @@
+import {
+  Timestamp,
+  addDoc,
+  arrayRemove,
+  arrayUnion,
+  collection,
+  doc,
+  setDoc,
+  updateDoc,
+} from 'firebase/firestore';
 import api from 'src/api/api';
+import {db} from '../../firebaseConfig';
 
 export const fetchParties = async () => {
   try {
     const res = await api.get({
       url: 'parties',
+      authorization: true,
+    });
+    return res.data.parties;
+  } catch (error) {
+    return error;
+  }
+};
+
+export const fetchPartyDetails = async (partyId: string) => {
+  try {
+    const res = await api.get({
+      url: `parties/party-details/${partyId}`,
       authorization: true,
     });
     return res.data.parties;
@@ -21,5 +44,98 @@ export const fetchPosts = async () => {
     return res?.data?.posts;
   } catch (error) {
     return error;
+  }
+};
+
+const partyCollection = collection(db, 'party');
+
+export const createFireStoreParties = async (data: {
+  partyId: string;
+  artist: string;
+  partyName: string;
+  partyType: string;
+}) => {
+  const party = {
+    partyId: data?.partyId,
+    artist: data?.artist,
+    partyName: data?.partyName,
+    partyType: data?.partyType,
+  };
+  // Use setDoc with a specific document ID
+  const partyRef = doc(partyCollection, data?.partyId);
+  try {
+    const response = await setDoc(partyRef, party);
+    console.log('response', response);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export type FireStoreComments = {
+  commentId: string;
+  userId: string;
+  text: string;
+  imageUri: string;
+  likes: string[];
+  timestamp: string;
+};
+
+export const createFireStoreComments = async (
+  partyId: string,
+  userId: string,
+  text: string,
+  imageUri: string,
+) => {
+  // Reference to the specific party's comments subcollection
+  const commentCollection = collection(db, `party/${partyId}/comments`);
+
+  // Creating the comment object
+  const comment = {
+    partyId,
+    userId,
+    timestamp: Timestamp.fromDate(new Date()),
+    text,
+    imageUri,
+    likes: [],
+  };
+
+  // Adding the comment to the subcollection
+  try {
+    const response = await addDoc(commentCollection, comment);
+    console.log('response', response);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const likeComment = async (
+  partyId: string,
+  commentId: string,
+  userId: string,
+) => {
+  const commentDoc = doc(db, `party/${partyId}/comments/${commentId}`);
+
+  try {
+    await updateDoc(commentDoc, {
+      likes: arrayUnion(userId),
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const unlikeComment = async (
+  partyId: string,
+  commentId: string,
+  userId: string,
+) => {
+  const commentDoc = doc(db, `party/${partyId}/comments/${commentId}`);
+
+  try {
+    await updateDoc(commentDoc, {
+      likes: arrayRemove(userId),
+    });
+  } catch (error) {
+    console.log(error);
   }
 };
