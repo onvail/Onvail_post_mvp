c48cc639c71aba84084d26c936bb1fcd12034a11
marvelous-ikechi <marvelike7@gmail.com>
2024-06-03 13:40:00 +0100

    navigate to home option added

--- src/app/components/Screens/ScreenContainer.tsx
+++ src/app/components/Screens/ScreenContainer.tsx
@@ -1,23 +1,26 @@
 import React, {FunctionComponent} from 'react';
-import {Platform, SafeAreaView, StatusBar, View} from 'react-native';
+import {Platform, Pressable, SafeAreaView, StatusBar, View} from 'react-native';
 import tw from 'lib/tailwind';
 import Icon from 'components/Icons/Icon';
 import RowContainer from 'components/View/RowContainer';
 import CustomText from 'components/Text/CustomText';
 import {useNavigation} from '@react-navigation/native';
+import {BottomTabParamList} from 'src/app/navigator/types/BottomTabParamList';
 
 interface Props {
   children: React.ReactNode;
   goBack?: boolean;
+  goHome?: boolean;
   screenHeader?: string;
 }
 
 const ScreenContainer: FunctionComponent<Props> = ({
   children,
   goBack,
+  goHome,
   screenHeader,
 }: Props) => {
-  const navigation = useNavigation();
+  const navigation = useNavigation<BottomTabParamList | any>();
   const marginForAndroid = Platform.OS === 'android' ? 10 : 0;
   return (
     <View style={tw`bg-primary flex-1 pt-${marginForAndroid}`}>
@@ -31,6 +34,16 @@ const ScreenContainer: FunctionComponent<Props> = ({
             color="white"
             size={30}
           />
+        ) : null}
+        {goHome ? (
+          <Pressable
+            onPress={() => {
+              navigation.navigate('BottomTabNavigator', {
+                screen: 'Home',
+              });
+            }}>
+            <Icon icon={'chevron-left'} color="white" size={30} />
+          </Pressable>
         ) : null}
         {screenHeader ? (
           <CustomText style={tw`text-lg`}>{screenHeader}</CustomText>
