429fae84dab94a6041c224ed407233ca57f80283
marvelous-ikechi <marvelike7@gmail.com>
2024-06-10 12:24:34 +0100

    UI redesigned

--- src/app/components/Cards/CommentCards.tsx
+++ src/app/components/Cards/CommentCards.tsx
@@ -4,14 +4,15 @@ import tw from 'src/lib/tailwind';
 import RowContainer from '../View/RowContainer';
 import Icon from '../Icons/Icon';
 import {Colors} from 'src/app/styles/colors';
-import {Pressable, TouchableOpacity, View} from 'react-native';
-import CustomImage from '../Image/CustomImage';
+import {TouchableOpacity, View} from 'react-native';
 import {
   FireStoreComments,
   likeComment,
   unlikeComment,
 } from 'src/actions/parties';
 import useUser from 'src/app/hooks/useUserInfo';
+import {MediaStream, mediaDevices} from 'react-native-webrtc';
+import {Avatar} from 'react-native-paper';
 
 const CommentCards: FunctionComponent<{
   item: FireStoreComments;
@@ -19,6 +20,7 @@ const CommentCards: FunctionComponent<{
 }> = ({item, partyId}) => {
   const [isMuted, setIsMuted] = useState<boolean>(false);
   const [isLiked, setIsLiked] = useState<boolean>(false);
+  const localStream = new MediaStream();
   const {user} = useUser();
 
   useEffect(() => {
@@ -39,41 +41,62 @@ const CommentCards: FunctionComponent<{
     }
   };
 
+  const toggleMute = async () => {
+    setIsMuted(!isMuted);
+    const audioTrack = localStream.getAudioTracks()[0];
+
+    // audioTrack.enabled = !audioTrack.enabled;
+  };
+
   return (
-    <View style={tw`flex-1 border-grey4 border-b-[0.2px] py-3 px-4`}>
+    <View style={tw`flex-1  py-3 px-4`}>
       <RowContainer style={tw`flex-1 flex-row justify-between   `}>
         <RowContainer>
-          {item?.imageUri ? (
-            <CustomImage
-              uri={item?.imageUri}
-              style={tw`h-5 w-5 rounded-full`}
-            />
-          ) : (
-            <Icon icon={'account'} color="white" size={20} />
-          )}
-          <CustomText style={tw`text-[10px] ml-5 w-[75%]`}>
-            {item?.text}
-          </CustomText>
-        </RowContainer>
-        <Pressable onPress={() => setIsMuted(!isMuted)}>
-          <Icon
-            icon={isMuted ? 'microphone' : 'microphone-off'}
+          <Avatar.Text
+            label={user?.name?.substring(0, 1) ?? ''}
+            size={27}
+            style={tw`bg-yellow1 `}
+            labelStyle={tw`font-poppinsBold text-base text-darkGreen`}
             color={Colors.white}
-            size={25}
           />
-        </Pressable>
-      </RowContainer>
-      <RowContainer style={tw`mt-4`}>
-        <TouchableOpacity onPress={() => handleLike()} style={tw` ml-12`}>
+          <View style={tw`w-[82%]`}>
+            <CustomText style={tw`text-[10px] ml-5 w-full`}>
+              @{item?.userStageName}
+            </CustomText>
+            <CustomText style={tw`text-[10px] w-full ml-5`}>
+              {item?.text}
+            </CustomText>
+          </View>
+        </RowContainer>
+        <TouchableOpacity onPress={() => handleLike()} style={tw``}>
           <Icon
-            icon={isLiked ? 'heart' : 'heart-outline'}
-            color={Colors.red}
+            icon={'heart'}
+            color={isLiked ? Colors.red : 'gray'}
             size={20}
           />
         </TouchableOpacity>
-        <CustomText style={tw`ml-2 text-xs`}>
-          {item?.likes?.length > 0 ? item?.likes?.length : null}
-        </CustomText>
+      </RowContainer>
+      <RowContainer style={tw`mt-1`}>
+        <RowContainer>
+          <TouchableOpacity onPress={() => handleLike()} style={tw` ml-12`}>
+            <Icon icon={'heart'} color={Colors.grey} size={13} />
+          </TouchableOpacity>
+          <CustomText style={tw`ml-1 text-[10px]`}>
+            {item?.likes?.length > 0 ? item?.likes?.length : null}
+          </CustomText>
+        </RowContainer>
+        <RowContainer>
+          <TouchableOpacity onPress={() => handleLike()} style={tw` ml-5`}>
+            <Icon icon={'reply'} color={Colors.grey} size={13} />
+          </TouchableOpacity>
+          <CustomText style={tw`ml-1 text-[10px]`}>Reply</CustomText>
+        </RowContainer>
+        <RowContainer>
+          <TouchableOpacity onPress={() => handleLike()} style={tw` ml-5`}>
+            <Icon icon={'comment-outline'} color={Colors.grey} size={13} />
+          </TouchableOpacity>
+          <CustomText style={tw`ml-1 text-[10px]`}>10</CustomText>
+        </RowContainer>
       </RowContainer>
     </View>
   );
