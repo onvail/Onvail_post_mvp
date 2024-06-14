4a61b615bee2d01e00bb8fa2cab175bcfdaf7ceb
marvelous-ikechi <marvelike7@gmail.com>
2024-06-14 12:30:48 +0100

    added country, state, and city dropdowns

--- src/app/auth/screens/LocationInput.tsx
+++ src/app/auth/screens/LocationInput.tsx
@@ -1,9 +1,13 @@
-import React, {FunctionComponent} from 'react';
+import React, {
+  FunctionComponent,
+  useCallback,
+  useEffect,
+  useState,
+} from 'react';
 import {TouchableOpacity, View} from 'react-native';
 import {useForm, Controller} from 'react-hook-form';
 import CustomText from 'app/components/Text/CustomText';
 import tw from 'lib/tailwind';
-import CustomTextInput from 'app/components/TextInput/CustomTextInput';
 import {AuthStackParamList} from 'src/app/navigator/types/AuthStackParamList';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import ErrorText from 'src/app/components/Text/ErrorText';
@@ -11,19 +15,82 @@ import RowContainer from 'src/app/components/View/RowContainer';
 import Icon from 'src/app/components/Icons/Icon';
 import AuthScreenContainer from 'src/app/components/Screens/AuthScreenContainer';
 import {SignUpStoreState, useSignUpStore} from 'src/app/zustand/store';
+import axios from 'axios';
+import CustomDropDown from 'src/app/components/Dropdown/CustomDropDown';
 
 type Props = NativeStackScreenProps<AuthStackParamList, 'Signup'>;
 
 type InitialValue = {
   country: string;
   city: string;
+  state: string;
 };
+
+interface State {
+  name: string;
+  state_code: string;
+}
+
+interface Country {
+  name: string;
+  iso3: string;
+  iso2: string;
+  states: State[];
+}
 
 const LocationInput: FunctionComponent<Props> = ({navigation}) => {
   const defaultValues: InitialValue = {
     country: '',
     city: '',
+    state: '',
   };
+
+  const [countriesAndStates, setCountriesAndStates] = useState<Country[]>([]);
+  const [selectedCountry, setSelectedCountry] = useState<Country | null>(null);
+  const [selectedState, setSelectedState] = useState<State | null>(null);
+  const [cities, setCities] = useState<string[]>([]);
+
+  const fetchAllCountries = async () => {
+    try {
+      const response = await axios.get(
+        'https://countriesnow.space/api/v0.1/countries/states',
+      );
+      setCountriesAndStates(response?.data?.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const fetchCitiesInAState = useCallback(async () => {
+    if (!selectedCountry || !selectedState) {
+      return;
+    }
+    try {
+      const response = await axios.post(
+        'https://countriesnow.space/api/v0.1/countries/state/cities',
+        {
+          country: selectedCountry?.name,
+          state: selectedState?.name,
+        },
+      );
+      setCities(
+        response?.data?.data.map((item: string) => ({
+          name: item,
+          key: item,
+        })),
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  }, [selectedCountry, selectedState]);
+
+  useEffect(() => {
+    fetchAllCountries();
+  }, []);
+
+  useEffect(() => {
+    fetchCitiesInAState();
+  }, [fetchCitiesInAState, selectedState, setSelectedCountry]);
 
   const updateUserSignUpStore = useSignUpStore(
     (state: SignUpStoreState) => state.updateUserSignUpStore,
@@ -39,11 +106,12 @@ const LocationInput: FunctionComponent<Props> = ({navigation}) => {
     mode: 'all',
   });
 
-  const onSubmit = async ({country, city}: InitialValue) => {
+  const onSubmit = async ({country, city, state}: InitialValue) => {
     updateUserSignUpStore({
       ...storeUser,
       country,
-      state: city,
+      state,
+      city,
     });
     navigation.navigate('RoleInput');
   };
@@ -71,14 +139,46 @@ const LocationInput: FunctionComponent<Props> = ({navigation}) => {
               required: 'Country is required',
             }}
             render={({field: {onChange, onBlur, value}}) => (
-              <CustomTextInput
-                placeholder="Country"
-                onChangeText={onChange}
+              <CustomDropDown
+                data={countriesAndStates}
+                onChange={item => {
+                  onChange(item.name);
+                  setSelectedCountry(item);
+                }}
+                labelField={'name'}
+                valueField={'name'}
+                maxHeight={250}
                 value={value}
                 onBlur={onBlur}
+                placeholder="Select country"
               />
             )}
             name="country"
+          />
+        </View>
+        <View>
+          <ErrorText>{errors?.state?.message}</ErrorText>
+          <Controller
+            control={control}
+            rules={{
+              required: 'State/Province is required',
+            }}
+            render={({field: {onChange, onBlur, value}}) => (
+              <CustomDropDown
+                data={selectedCountry?.states || []}
+                onChange={item => {
+                  onChange(item.name);
+                  setSelectedState(item);
+                }}
+                labelField={'name'}
+                valueField={'name'}
+                maxHeight={250}
+                value={value}
+                onBlur={onBlur}
+                placeholder="Select state/province"
+              />
+            )}
+            name="state"
           />
         </View>
         <View>
@@ -89,11 +189,18 @@ const LocationInput: FunctionComponent<Props> = ({navigation}) => {
               required: 'City is required',
             }}
             render={({field: {onChange, onBlur, value}}) => (
-              <CustomTextInput
-                placeholder="City"
-                onChangeText={onChange}
+              <CustomDropDown
+                data={cities}
+                onChange={item => {
+                  onChange(item.name);
+                  setSelectedState(item);
+                }}
+                labelField={'name'}
+                valueField={'name'}
+                maxHeight={250}
                 value={value}
                 onBlur={onBlur}
+                placeholder="City"
               />
             )}
             name="city"
