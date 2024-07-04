f5a8fdaf36531fcc3d871f60c477f7e45e944903
marvelous-ikechi <marvelike7@gmail.com>
2024-07-04 11:50:59 +0100

    added new environment keys

--- src/types/env.d.tsx
+++ src/types/env.d.tsx
@@ -2,4 +2,6 @@ declare module '@env' {
   export const BASE_URL: string;
   export const CLOUD_NAME: string;
   export const UPLOAD_PRESET: string;
+  export const AWS_ACCESSKEY_ID: string;
+  export const AWS_SECRET_ACCESS_KEY: string;
 }
